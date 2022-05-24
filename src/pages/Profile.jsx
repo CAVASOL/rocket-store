40ec57f570bc6603173b4e028fedb8e0f006df8e
Yeon Kim <awyeon@gmail.com>
2022-05-24 20:25:17 +0800

    added page and tested

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { getAuth } from 'firebase/auth'
+import { useState, useEffect } from 'react'
 
 function Profile() {
+  const [user, setUser] = useState(null)
+
+  const auth = getAuth()
+
+  useEffect(() => {
+    setUser(auth.currentUser)
+  }, [])
+
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    user ? <h1>{user.displayName}</h1> : 'Not Logged In'
   )
 }
 
