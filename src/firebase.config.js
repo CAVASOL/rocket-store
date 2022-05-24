c4455c37e97cb6d49f03a862af63afef49a3cf27
Yeon Kim <awyeon@gmail.com>
2022-05-24 13:21:21 +0800

    initial commit

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDHhkq6l6YXgg3A9Mx0S7Deg4Bse0xHRUA",
+  authDomain: "rocket-store-app.firebaseapp.com",
+  projectId: "rocket-store-app",
+  storageBucket: "rocket-store-app.appspot.com",
+  messagingSenderId: "756809617384",
+  appId: "1:756809617384:web:327f8f0cf5b7f572161e5d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
