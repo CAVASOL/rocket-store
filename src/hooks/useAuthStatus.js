868c9743b96b843b8f849cb8b9e32ba1f92b38a5
Yeon Kim <awyeon@gmail.com>
2022-05-25 17:45:57 +0800

    tested

--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -1,19 +1,30 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStatus = () => {
   const [loggedIn, setLoggedIn] = useState(false)
   const [checkingStatus, setCheckingStatus] = useState(true)
+  const isMounted = useRef(true)
 
   useEffect(() => {
-    const auth = getAuth()
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setLoggedIn(true)
-      }
-      setCheckingStatus(false)
-    })
-  })
+    if (isMounted) {
+      const auth = getAuth()
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
+        }
+        setCheckingStatus(false)
+      })
+    }
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [isMounted])
 
   return { loggedIn, checkingStatus }
 }
+
+
+// Protected routes in v6 - stackoverflow q.65505665
+// Fix memory leak warning - stackoverflow q.59780268
