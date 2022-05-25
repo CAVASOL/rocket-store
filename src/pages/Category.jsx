82e00b9c35cc58a31d5a4f568671e24d6c5f3cba
Yeon Kim <awyeon@gmail.com>
2022-05-25 22:22:48 +0800

    added an item

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { collection, getDoc, query, where, orderBy, limit, startAfter, getDocs } from 'firebase/firestore'
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
 import { db } from '../firebase.config'
 import { toast } from 'react-toastify'
 import Spinner from '../components/Spinner'
