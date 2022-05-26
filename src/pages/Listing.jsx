e9124c9fb3febddb9c7ab9aacbf93ffeb8e1a78b
Yeon Kim <awyeon@gmail.com>
2022-05-26 21:22:45 +0800

    added

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -22,7 +22,6 @@ function Listing() {
       const docSnap = await getDoc(docRef)
 
       if(docSnap.exists()) {
-        console.log(docSnap.data())
         setListing(docSnap.data())
         setLoading(false)
       }
@@ -97,9 +96,7 @@ function Listing() {
 
         {auth.currentUser?.uid !== listing.userRef && (
           <Link
-            to={`/contact/${listing.userRef}?
-            listingName=${listing.name}&
-            listingLocation=${listing.location}`}
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
             className='primaryButton'
           >
             Contact Landlord
