be58ab6297de30fdda8d537e492ab660dc0f33a5
CAVASOL <awyeon@gmail.com>
2023-04-05 21:52:16 +0800

    test

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHhkq6l6YXgg3A9Mx0S7Deg4Bse0xHRUA",
-  authDomain: "rocket-store-app.firebaseapp.com",
-  projectId: "rocket-store-app",
-  storageBucket: "rocket-store-app.appspot.com",
-  messagingSenderId: "756809617384",
-  appId: "1:756809617384:web:327f8f0cf5b7f572161e5d"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 initializeApp(firebaseConfig);
-export const db = getFirestore()
+export const db = getFirestore();
